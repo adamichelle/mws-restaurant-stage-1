d3869598140573d48aa6b35a1a39f7649477cbea
Aniuchi Adaobi Michelle <adamichelllle@gmail.com>
2019-05-06 14:41:32 +0100

    Change 'database-urls' to hosted server url.

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -40,8 +40,8 @@ class DBHelper {
     // return `http://localhost:${port}/data/restaurants.json`;
     // return `./data/restaurants.json`;
     const databaseUrls = [
-      `http://localhost:1337/restaurants/`,
-      `http://localhost:1337/reviews/`
+      `https://my-restaurant-reviews-backend.herokuapp.com/restaurants/`,
+      `https://my-restaurant-reviews-backend.herokuapp.com/reviews/`
     ]
     return databaseUrls;
   }
